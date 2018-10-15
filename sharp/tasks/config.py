0057749afb6bf86ba6c535d7e0826e652b3d1472
Tomas Fiers <tomas.fiers@gmail.com>
2018-10-15 13:41:36 +0200

    Do not make CNSN-poster

--- sharp/tasks/config.py
+++ sharp/tasks/config.py
@@ -4,17 +4,26 @@ from sharp.tasks.plot.base import MultiEnvelopeFigureMaker
 from sharp.tasks.plot.signals.reference import PlotReferenceMaker
 from sharp.tasks.signal.online_bpf import ApplyOnlineBPF
 
-TASKS_TO_RUN = (
-    PlotReferenceMaker(),
-    MultiEnvelopeFigureMaker(
-        combi_ID="CNSN-poster", envelope_makers=(ApplyOnlineBPF(), ApplyRNN())
-    ),
-    MultiEnvelopeFigureMaker(
-        combi_ID="chapter-multi-lin",
-        envelope_makers=(
-            ApplyOnlineBPF(),
-            SpatiotemporalConvolution(delays=0),
-            SpatiotemporalConvolution(delays=3),
-        ),
+# fmt: off
+CNSN_poster = MultiEnvelopeFigureMaker(
+    combi_ID="CNSN-poster",
+    envelope_makers=(
+        ApplyOnlineBPF(),
+        ApplyRNN(),
+    )
+)
+
+chapter_multi_lin = MultiEnvelopeFigureMaker(
+    combi_ID="chapter-multi-lin",
+    envelope_makers=(
+        ApplyOnlineBPF(),
+        SpatiotemporalConvolution(delays=0),
+        SpatiotemporalConvolution(delays=3),
     ),
 )
+
+TASKS_TO_RUN = (
+    PlotReferenceMaker(),
+    chapter_multi_lin,
+)
+# fmt: on
