9db94c203b6b3bde2cda315106bc36c02be76b93
Tomas Fiers <tomas.fiers@gmail.com>
2018-10-22 04:23:15 +0200

    Type hints for subplots output.

--- sharp/data/types/aliases.py
+++ sharp/data/types/aliases.py
@@ -2,12 +2,14 @@
 Aliases of common types, for readability & consistency of type-hints.
 """
 
-from typing import Union
+from typing import Sequence, Tuple, Union
 
-import matplotlib.axes
-import matplotlib.figure
 import numpy
 import torch
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+
 
 TorchArray = torch.Tensor
 NeuralModel = torch.nn.Module
@@ -17,3 +19,7 @@ EventList = numpy.ndarray  # A 1d array of event times, in seconds.
 BooleanArray = numpy.ndarray
 Scalar = Union[complex, float, int, bool]
 ArrayLike = Union[numpy.ndarray, list, tuple, Scalar]
+
+
+def subplots(*args, **kwargs) -> Tuple[Figure, Union[Axes, Sequence[Axes]]]:
+    return plt.subplots(*args, **kwargs)
