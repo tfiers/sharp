ce96758ba4f9f19e2294dadaaef7c628303d28e0
Tomas Fiers <tomas.fiers@gmail.com>
2019-05-10 14:01:26 +0200

    Fix extension dot bug. Error on non-supported raw fileformat

--- sharp/tasks/signal/raw.py
+++ sharp/tasks/signal/raw.py
@@ -1,3 +1,5 @@
+from logging import getLogger
+
 from sharp.config.load import config
 from sharp.data.files.raw_data import (
     RawDATFile,
@@ -8,6 +10,8 @@ from sharp.data.files.raw_data import (
 from sharp.data.types.config import RecordingFileID
 from sharp.tasks.base import CustomParameter, ExternalTask, SharpTask
 
+log = getLogger(__name__)
+
 
 class SingleRecordingFileTask(SharpTask):
     file_ID: RecordingFileID = CustomParameter()
@@ -16,14 +20,20 @@ class SingleRecordingFileTask(SharpTask):
 class RawRecording_ExistenceCheck(ExternalTask, SingleRecordingFileTask):
     def output(self) -> RawRecording:
         path = self.file_ID.path
-        extension = path.suffix.lower()
+        filename, extension = path.name.split(".", 1)
+        extension = "." + extension
         possible_classes = (RawDATFile, RawKwikFile, TahitiFile)
-        FileClass = next(
-            C for C in possible_classes if C.extension == extension
-        )
-        return FileClass(
-            directory=path.parent, filename=path.name.split(".")[0]
-        )
+        try:
+            FileClass = next(
+                C for C in possible_classes if C.extension == extension
+            )
+        except StopIteration:
+            log.error(
+                f"{self.file_ID} ({self.file_ID.path}) is not one of the"
+                f" accepted file formats"
+                f" ({', '.join(C.extension for C in possible_classes)})."
+            )
+        return FileClass(directory=path.parent, filename=filename)
 
 
 class CheckWhetherAllRawRecordingsExist(SharpTask):
