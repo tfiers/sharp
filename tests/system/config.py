3d44cd254a3b7b909a05f74dbf0c3b4693f5e696
Tomas Fiers <tomas.fiers@gmail.com>
2019-05-22 19:32:08 +0200

    Update test config.py to new format (all arguments required)

--- tests/system/config.py
+++ tests/system/config.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 
+from numpy import linspace
+from sharp.config.default.logging import LOGGING_CONFIG
+from sharp.config.default.tasks import get_default_tasks
 from sharp.config.spec import SharpConfig
 from sharp.config.types import RecordingFileID
 
 
 config = SharpConfig(
-    # [Data]
+    get_tasks=get_default_tasks,
     raw_data=(
         RecordingFileID(
             rat=2, day=5, probe="D29", path=Path("data/raw/sig.moz")
@@ -19,12 +22,27 @@ config = SharpConfig(
     ),
     output_dir="data/processed",
     shared_output_dir="data/processed-shared",
-    # [Main]
+    fs_target=1000,
+    bitmap_versions=False,
+    logging=LOGGING_CONFIG,
+    scheduler_url=None,
     config_id="test",
-    num_thresholds=6,
-    time_ranges=[(0.68, 1.2)],
-    # [NeuralNet]
-    num_layers=1,
-    num_units_per_layer=8,
-    num_epochs=2,
+    mult_detect_ripple=tuple(linspace(0.4, 4, num=7)),
+    mult_detect_SW=tuple(linspace(0.9, 5, num=7)),
+    lockout_time=60e-3,
+    num_thresholds=64,
+    train_fraction=0.6,
+    train_first=True,
+    eval_start_extension=14e-3,
+    num_layers=2,
+    num_units_per_layer=40,
+    chunk_duration=0.3,
+    p_dropout=0.4,
+    num_epochs=15,
+    valid_fraction=0.22,
+    pos_weight=1.0,
+    target_fullrect=False,
+    target_start_pre=14e-3,
+    target_start_post=25e-3,
+    reference_seg_extension=0,
 )
